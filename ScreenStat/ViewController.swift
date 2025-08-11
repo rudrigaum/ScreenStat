7a37c96f92bd3a6a4a4849bb7968f84b0493458e
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-08-11 16:34:02 -0300

    Initial Commit

---
+++ ScreenStat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenStat
+//
+//  Created by Rodrigo Cerqueira Reis on 11/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
